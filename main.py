9a831f9dd51324bfe5b953c3319b0b77e5365931
Klimchenkov <klimchenkoff@mail.ru>
2023-02-07 13:32:34 +0300

    Initial

---
+++ main.py
@@ -0,0 +1,20 @@
+import sys
+
+class ReadExcel:
+    
+    def __init__(self, path_to_file:str):
+        
+        self.path_to_file = path_to_file
+
+        
+        
+        
+        
+        
+        
+        
+
+
+
+if __name__ == '__main__':
+    ReadExcel(sys.argv[1])
