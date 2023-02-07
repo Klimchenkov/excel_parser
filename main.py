a5aa6418e3df07c526d8e62a962e80663bdd9ebd
Klimchenkov <klimchenkoff@mail.ru>
2023-02-07 19:12:30 +0300

    update

--- main.py
+++ main.py
@@ -4,19 +4,21 @@ import sqlite3 as sl
 from openpyxl import load_workbook
 
 class ReadExcel:
+
+    TABLE_NAME = 'parsed_data'
     
-    def __init__(self, path_to_file:str):
-        
+    def __init__(self, path_to_file:str, parse:str):
         self.path_to_file = path_to_file
         self.con = sl.connect('excel_parser.db')
         self._create_db()
-        self._parse_file()
+        if parse == 'true':
+            self._parse_file()
+        self._get_total()
         
     def _create_db(self):
         with self.con as con:
-            sql = """
-            CREATE TABLE IF NOT EXISTS parsed_data (
-                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            sql = f"""
+            CREATE TABLE IF NOT EXISTS {self.TABLE_NAME} (
                 value INTEGER NOT NULL,
                 date STRING,
                 type STRING,
@@ -25,13 +27,33 @@ class ReadExcel:
             );
             """
             con.execute(sql)
-    
+
     def _parse_file(self):
-        print(self.path_to_file)
+        wb = load_workbook(self.path_to_file)
+        sheet = wb.worksheets[0]
+        for r in range(4, sheet.max_row + 1):
+            for c in range(3, 11): 
+                value = sheet.cell(row=r, column=c).value
+                date = sheet.cell(row=3, column=c).value
+                type = sheet.cell(row=2, column=c if c%2!=0 else c - 1).value
+                company = sheet.cell(row=r, column=2).value
+                timing = sheet.cell(row=1, column=3 if c in range(3,7) else 7).value
+                tup = (value, date, type, company, timing)
+                with self.con as con:
+                    sql=f"""
+                    INSERT INTO {self.TABLE_NAME} VALUES {tup};
+                    """
+                    con.execute(sql)
+
+    def _get_total(self):
         
-        
-
-
-
+        with self.con as con:
+            sql = 'SELECT date, timing, SUM(value) from parsed_data group by date, timing;'
+            res = con.execute(sql)
+            print('\nРезультат:\n')
+            for i in res:
+                print(f'Рассчётный {"" if i[1] == "fact" else "прогнозный "}тотал для {i[0]} - {i[2]}.')
+            print('\n')
+            
 if __name__ == '__main__':
-    ReadExcel(sys.argv[1])
+    ReadExcel(sys.argv[1], sys.argv[2])
--- script.sh
+++ script.sh
@@ -3,8 +3,9 @@
 if [ ! -d ./venv ]; then
   python3 -m venv venv
 fi
-# source venv/bin/activate
+source venv/bin/activate
+pip install -r requirements.txt
 
-python -m main './data.xlsx'
+python -m main './data.xlsx' 'true'
 
-# deactivate
+deactivate
