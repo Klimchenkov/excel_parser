94d5ef2d1fda30ab25f2876a24720483d9f2976d
Klimchenkov <klimchenkoff@mail.ru>
2023-02-07 15:42:18 +0300

    update

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 import sys
 
+import sqlite3 as sl
+from openpyxl import load_workbook
+
 class ReadExcel:
     
     def __init__(self, path_to_file:str):
         
         self.path_to_file = path_to_file
-
+        self.con = sl.connect('excel_parser.db')
+        self._create_db()
+        self._parse_file()
         
-        
-        
-        
-        
+    def _create_db(self):
+        with self.con as con:
+            sql = """
+            CREATE TABLE IF NOT EXISTS parsed_data (
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                value INTEGER NOT NULL,
+                date STRING,
+                type STRING,
+                company STRING,
+                timing STRING
+            );
+            """
+            con.execute(sql)
+    
+    def _parse_file(self):
+        print(self.path_to_file)
         
         
 
--- script.sh
+++ script.sh
@@ -3,8 +3,8 @@
 if [ ! -d ./venv ]; then
   python3 -m venv venv
 fi
-source venv/bin/activate
+# source venv/bin/activate
 
 python -m main './data.xlsx'
 
-deactivate
+# deactivate
